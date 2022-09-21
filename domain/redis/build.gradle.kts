1f0a5e026f7b3abf509cd225556840df09743a43
logan-beproud <logan.beproud@krosslab.io>
2022-09-21 16:36:51 +0900

    domain redis boot jar false

--- domain/redis/build.gradle.kts
+++ domain/redis/build.gradle.kts
@@ -1,6 +1,17 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 }
 
+tasks {
+    withType<BootJar> {
+        enabled = false
+    }
+
+    withType<Jar> {
+        enabled = true
+    }
+}
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
