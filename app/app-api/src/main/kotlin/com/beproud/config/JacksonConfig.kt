95a424a20a03fa8ce3cc1a06c6e8469f1f76e0ca
logan-beproud <logan.beproud@krosslab.io>
2022-09-21 18:33:43 +0900

    Update JacksonConfig.kt

--- app/app-api/src/main/kotlin/com/beproud/config/JacksonConfig.kt
+++ app/app-api/src/main/kotlin/com/beproud/config/JacksonConfig.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,6 +15,8 @@ class JacksonConfig {
     fun objectMapper(): ObjectMapper =
             jacksonObjectMapper()
                     .findAndRegisterModules()
+                    .registerKotlinModule()
                     .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
                     .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                    .configure(SerializationFeature.WRITE_DURATIONS_AS_TIMESTAMPS, false)
 }
