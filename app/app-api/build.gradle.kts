51830dc9bcd6fec1cb07526b0b90b26984a89dce
logan-beproud <logan.beproud@krosslab.io>
2022-09-21 11:54:20 +0900

    app-api 설정 변경

--- app/app-api/build.gradle.kts
+++ app/app-api/build.gradle.kts
@@ -28,6 +28,8 @@ configurations {
 }
 
 dependencies {
+    implementation(project(":domain:rds"))
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
